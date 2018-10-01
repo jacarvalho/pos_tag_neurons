fdccb315b929efaf6501c79fa6e7e36bc007b851
joaocarvalho <joaoaccarvalho@gmail.com>
2018-10-01 18:13:19 +0200

    Does not install a virtual environment when running setup_directory.sh

--- setup_directory.sh
+++ setup_directory.sh
@@ -1,10 +1,14 @@
-# Run this script to install the required packages in a virtual environment
-virtualenv -p python3 venv
-source venv/bin/activate
-pip3 install --upgrade setuptools pip
-pip3 install -r requirements.txt
-python3 setup.py install
-deactivate
+# Run this script to install the required packages in a virtual environment.
+# Umcomment the following lines to install a virtual environment with all
+# libraries needed in your machine.
+
+# virtualenv -p python3 venv
+# source venv/bin/activate
+# pip3 install --upgrade setuptools pip
+# pip3 install -r requirements.txt
+# python3 setup.py install
+# deactivate
+
 
 # Concatenate trained lstm models together
 cat byte_LSTM_trained_models/amazon/save/806/model-1927169.data-00000-of-00001.a* > byte_LSTM_trained_models/amazon/save/806/model-1927169.data-00000-of-00001
